f6769e17cf8bc45e32cb3935c8115a9639387c43
C108118206 <C108118206@nkust.edu.tw>
2022-06-10 14:49:32 +0800

    附檔名hint

--- resources/views/backstage/customer/backstage_customer_diy.blade.php
+++ resources/views/backstage/customer/backstage_customer_diy.blade.php
@@ -110,7 +110,7 @@
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     </div>
                     <div class="">
-                        <label for="file" class="mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">D I Y 文件</label>
+                        <label for="file" class="mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">D I Y 文件 <br><span class="text-red-500">副檔名(doc ,pdf)  限制大小為2Mb</label>
                         <input name="file" type="file" id="file"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             >
